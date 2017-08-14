4c2f83d71990143e16887f974b2077b54a24e6c2
Subtronic <crashmybrain@gmail.com>
2017-08-15 00:45:53 +0300

    Fix test

--- test/basic.js
+++ test/basic.js
@@ -2,12 +2,24 @@ var should = require('chai').should();
 var expect = require('chai').expect;
 var assert = require('chai').assert;
 var _ = require('../');
-require('./dgram/OrderBook')(_);
-require('./dgram/HeartBeat')(_);
 
 describe('#c-struct', function() {
   it('should correct work upack by types', function(done) {
-    
+        _.register('md_header', new _.Schema({
+        'system_time': _.type.time64,
+        'source_id': _.type.uint16
+    }));
+
+    _.register('frame', new _.Schema({
+        'size': _.type.uint16,
+        'msgid': _.type.uint16,
+        'seq': _.type.uint64
+    }));
+    _.register('HeartBeat', new _.Schema({
+        'frame': 'frame',
+        'md_header': 'md_header',
+        'reserved': _.type.uint32
+    }));
     var hb = new Buffer('0e00843b8f6f00000080c000636df9221bd4da142d0100000000');
     var hbUnpacked = _.unpackSync('HeartBeat', hb);
     hbUnpacked['msgid'].should.equal(12336);
